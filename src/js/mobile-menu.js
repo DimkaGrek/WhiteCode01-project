7e0b06dfe38f07c6c8665e0a49984388c893e18f
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2023-11-05 22:10:58 +0200

    add div backdrop

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const menuLinks = document.querySelectorAll('.mobile-menu-link'); // select all links in menu
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -17,6 +18,20 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Close the mobile menu on click any link
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      if (mobileMenu.classList.contains('is-open')) {
+        toggleMenu();
+      }
+    });
+  });
+
+  // Close menu when a link is clicked
+  // menuLinks.forEach(link => {
+  //   link.addEventListener('click', toggleMenu);
+  // });
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
